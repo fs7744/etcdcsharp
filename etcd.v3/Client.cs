2bc4fd5033e28f4ccc86a57691ba70109e019611
Victor.X.Qu <fs7744@hotmail.com>
2017-08-01 21:35:00 +0800

    add client

---
+++ etcd.v3/Client.cs
@@ -0,0 +1,72 @@
+﻿using Etcdserverpb;
+using Grpc.Core;
+using System.Threading.Tasks;
+using static Etcdserverpb.Auth;
+using static Etcdserverpb.Cluster;
+using static Etcdserverpb.KV;
+using static Etcdserverpb.Lease;
+using static Etcdserverpb.Maintenance;
+using static Etcdserverpb.Watch;
+
+namespace ETCD.V3
+{
+    public class Client
+    {
+        private Channel _Channel;
+        public AuthClient Auth { get; private set; }
+        public KVClient KV { get; private set; }
+        public ClusterClient Cluster { get; private set; }
+        public MaintenanceClient Maintenance { get; private set; }
+        public LeaseClient Lease { get; private set; }
+        public WatchClient Watch { get; private set; }
+
+        public Client(string target, string user = null, string password = null)
+        {
+            _Channel = new Channel(target, ChannelCredentials.Insecure);
+            Authebtucate(target, user, password);
+            InitClient();
+        }
+
+        private void Authebtucate(string target, string user, string password)
+        {
+            if (!string.IsNullOrWhiteSpace(user))
+            {
+                Auth = new AuthClient(_Channel);
+                var res = Auth.Authenticate(new AuthenticateRequest()
+                {
+                    Name = user,
+                    Password = password
+                });
+                var aai = CallCredentials.FromInterceptor((context, metadata) =>
+                {
+                    metadata.Add(new Metadata.Entry(Constants.Token, res.Token));
+                    return Task.CompletedTask;
+                });
+                var co = new CallOptions().WithCredentials(aai);
+                var cc = ChannelCredentials.Create(ChannelCredentials.Insecure, aai);
+                Close();
+                _Channel = new Channel(target, cc);
+            }
+        }
+
+        private void InitClient()
+        {
+            Auth = new AuthClient(_Channel);
+            KV = new KVClient(_Channel);
+            Cluster = new ClusterClient(_Channel);
+            Maintenance = new MaintenanceClient(_Channel);
+            Lease = new LeaseClient(_Channel);
+            Watch = new WatchClient(_Channel);
+        }
+
+        public Task CloseAsync()
+        {
+            return _Channel.ShutdownAsync();
+        }
+
+        public void Close()
+        {
+            CloseAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+    }
+}
---
+++ etcd.v3/Constants.cs
@@ -0,0 +1,11 @@
+﻿using Google.Protobuf;
+using System;
+
+namespace ETCD.V3
+{
+    public static class Constants
+    {
+        public const string Token = "token";
+        public static readonly ByteString NullKey = ByteString.CopyFrom(new byte[] { Convert.ToByte('\0') });
+    }
+}
