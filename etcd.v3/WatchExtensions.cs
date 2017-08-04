d4108c797fc418af23cc6f980c730328d6febc5f
Victor.X.Qu <victor.x.qu@newegg.com>
2017-08-04 11:39:56 +0800

    add watch

---
+++ etcd.v3/WatchExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Etcdserverpb;
+using Grpc.Core;
+
+namespace ETCD.V3
+{
+    public static class WatchExtensions
+    {
+        public static AsyncDuplexStreamingCall<WatchRequest, WatchResponse> Watch(this Client client)
+        {
+            return client.Watch.Watch(client.CallToken);
+        }
+    }
+}
