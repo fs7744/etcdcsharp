667f34b2cd1820daf39cf2feee5b54d9fa2a5d15
Victor.X.Qu <fs7744@hotmail.com>
2017-08-04 23:26:42 +0800

    add doc

--- etcd.v3/LeaseExtensions.cs
+++ etcd.v3/LeaseExtensions.cs
@@ -7,6 +7,12 @@ namespace ETCD.V3
     {
         #region LeaseGrant
 
+        /// <summary>
+        /// Create LeaseGrantRequest.
+        /// </summary>
+        /// <param name="ttl">TTL is the advisory time-to-live in seconds.</param>
+        /// <param name="id">ID is the requested ID for the lease. If ID is set to 0, the lessor chooses an ID.</param>
+        /// <returns>LeaseGrantRequest.</returns>
         public static LeaseGrantRequest CreateLeaseGrantRequest(this Client client, long ttl, long id)
         {
             return new LeaseGrantRequest()
@@ -16,12 +22,28 @@ namespace ETCD.V3
             };
         }
 
+        /// <summary>
+        /// LeaseGrant creates a lease which expires if the server does not receive a keepAlive
+        /// within a given time to live period. All keys attached to the lease will be expired and
+        /// deleted if the lease expires. Each expired key generates a delete event in the event history.
+        /// </summary>
+        /// <param name="ttl">TTL is the advisory time-to-live in seconds.</param>
+        /// <param name="id">ID is the requested ID for the lease. If ID is set to 0, the lessor chooses an ID.</param>
+        /// <returns>The response received from the server.</returns>
         public static LeaseGrantResponse LeaseGrant(this Client client, long ttl, long id)
         {
             var request = client.CreateLeaseGrantRequest(ttl, id);
             return client.Lease.LeaseGrant(request, client.AuthToken);
         }
 
+        /// <summary>
+        /// LeaseGrant creates a lease which expires if the server does not receive a keepAlive
+        /// within a given time to live period. All keys attached to the lease will be expired and
+        /// deleted if the lease expires. Each expired key generates a delete event in the event history.
+        /// </summary>
+        /// <param name="ttl">TTL is the advisory time-to-live in seconds.</param>
+        /// <param name="id">ID is the requested ID for the lease. If ID is set to 0, the lessor chooses an ID.</param>
+        /// <returns>The call object.</returns>
         public static AsyncUnaryCall<LeaseGrantResponse> LeaseGrantAsync(this Client client, long ttl, long id)
         {
             var request = client.CreateLeaseGrantRequest(ttl, id);
@@ -32,6 +54,11 @@ namespace ETCD.V3
 
         #region LeaseRevoke
 
+        /// <summary>
+        /// Create LeaseRevokeRequest.
+        /// </summary>
+        /// <param name="id">ID is the lease ID to revoke. When the ID is revoked, all associated keys will be deleted.</param>
+        /// <returns>The response received from the server.</returns>
         public static LeaseRevokeRequest CreateLeaseRevokeRequest(this Client client, long id)
         {
             return new LeaseRevokeRequest()
@@ -40,12 +67,22 @@ namespace ETCD.V3
             };
         }
 
+        /// <summary>
+        /// LeaseRevoke revokes a lease. All keys attached to the lease will expire and be deleted.
+        /// </summary>
+        /// <param name="id">ID is the lease ID to revoke. When the ID is revoked, all associated keys will be deleted.</param>
+        /// <returns>The response received from the server.</returns>
         public static LeaseRevokeResponse LeaseRevoke(this Client client, long id)
         {
             var request = client.CreateLeaseRevokeRequest(id);
             return client.Lease.LeaseRevoke(request, client.AuthToken);
         }
 
+        /// <summary>
+        /// LeaseRevoke revokes a lease. All keys attached to the lease will expire and be deleted.
+        /// </summary>
+        /// <param name="id">ID is the lease ID to revoke. When the ID is revoked, all associated keys will be deleted.</param>
+        /// <returns>The call object.</returns>
         public static AsyncUnaryCall<LeaseRevokeResponse> LeaseRevokeAsync(this Client client, long id)
         {
             var request = client.CreateLeaseRevokeRequest(id);
@@ -54,6 +91,11 @@ namespace ETCD.V3
 
         #endregion LeaseRevoke
 
+        /// <summary>
+        /// LeaseKeepAlive keeps the lease alive by streaming keep alive requests from the client
+        /// to the server and streaming keep alive responses from the server to the client.
+        /// </summary>
+        /// <returns>The call object.</returns>
         public static AsyncDuplexStreamingCall<LeaseKeepAliveRequest, LeaseKeepAliveResponse> LeaseKeepAlive(this Client client)
         {
             return client.Lease.LeaseKeepAlive(client.AuthToken);
@@ -61,6 +103,12 @@ namespace ETCD.V3
 
         #region LeaseTimeToLive
 
+        /// <summary>
+        /// Create LeaseTimeToLiveRequest.
+        /// </summary>
+        /// <param name="id">ID is the lease ID for the lease.</param>
+        /// <param name="keys">keys is true to query all the keys attached to this lease.</param>
+        /// <returns>LeaseTimeToLiveRequest.</returns>
         public static LeaseTimeToLiveRequest CreateLeaseTimeToLiveRequest(this Client client, long id, bool keys = false)
         {
             return new LeaseTimeToLiveRequest()
@@ -70,12 +118,24 @@ namespace ETCD.V3
             };
         }
 
+        /// <summary>
+        /// LeaseTimeToLive retrieves lease information.
+        /// </summary>
+        /// <param name="id">ID is the lease ID for the lease.</param>
+        /// <param name="keys">keys is true to query all the keys attached to this lease.</param>
+        /// <returns>The response received from the server.</returns>
         public static LeaseTimeToLiveResponse LeaseTimeToLive(this Client client, long id, bool keys = false)
         {
             var request = client.CreateLeaseTimeToLiveRequest(id, keys);
             return client.Lease.LeaseTimeToLive(request, client.AuthToken);
         }
 
+        /// <summary>
+        /// LeaseTimeToLive retrieves lease information.
+        /// </summary>
+        /// <param name="id">ID is the lease ID for the lease.</param>
+        /// <param name="keys">keys is true to query all the keys attached to this lease.</param>
+        /// <returns>The call object.</returns>
         public static AsyncUnaryCall<LeaseTimeToLiveResponse> LeaseTimeToLiveAsync(this Client client, long id, bool keys = false)
         {
             var request = client.CreateLeaseTimeToLiveRequest(id, keys);
--- etcd.v3/WatchExtensions.cs
+++ etcd.v3/WatchExtensions.cs
@@ -5,6 +5,14 @@ namespace ETCD.V3
 {
     public static class WatchExtensions
     {
+        /// <summary>
+        /// Watch watches for events happening or that have happened. Both input and output
+        /// are streams; the input stream is for creating and canceling watchers and the output
+        /// stream sends events. One watch RPC can watch on multiple key ranges, streaming events
+        /// for several watches at once. The entire event history can be watched starting from the
+        /// last compaction revision.
+        /// </summary>
+        /// <returns>The call object.</returns>
         public static AsyncDuplexStreamingCall<WatchRequest, WatchResponse> Watch(this Client client)
         {
             return client.Watch.Watch(client.AuthToken);
